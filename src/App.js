1494665476ed272dd72a14d7b109a834ff2a1544
Ruslan <djeko454@mail.ru>
2022-04-23 12:15:18 +0300

    sort_value

--- src/App.js
+++ src/App.js
@@ -1,149 +1,15 @@
 import './App.css';
 import axios from 'axios'
 import {useEffect, useMemo, useState} from "react";
+import {coinNames} from "./coin-names";
 
 
 export const App = () => {
 
     const [myAllCoins, setMyAllCoins] = useState([])
-    const coinNames = ['BTCUSDT',
-        'ETHUSDT',
-        'BCHUSDT',
-        'XRPUSDT',
-        'EOSUSDT',
-        'LTCUSDT',
-        'TRXUSDT',
-        'ETCUSDT',
-        'LINKUSDT',
-        'XLMUSDT',
-        'ADAUSDT',
-        'XMRUSDT',
-        'DASHUSDT',
-        'ZECUSDT',
-        'XTZUSDT',
-        'BNBUSDT',
-        'ATOMUSDT',
-        'ONTUSDT',
-        'IOTAUSDT',
-        'BATUSDT',
-        'VETUSDT',
-        'NEOUSDT',
-        'QTUMUSDT',
-        'IOSTUSDT',
-        'THETAUSDT',
-        'ALGOUSDT',
-        'ZILUSDT',
-        'KNCUSDT',
-        'ZRXUSDT',
-        'COMPUSDT',
-        'OMGUSDT',
-        'DOGEUSDT',
-        'SXPUSDT',
-        'KAVAUSDT',
-        'BANDUSDT',
-        'RLCUSDT',
-        'WAVESUSDT',
-        'MKRUSDT',
-        'SNXUSDT',
-        'DOTUSDT',
-        'YFIUSDT',
-        'BALUSDT',
-        'CRVUSDT',
-        'TRBUSDT',
-        'YFIIUSDT',
-        'RUNEUSDT',
-        'SUSHIUSDT',
-        'SRMUSDT',
-        'EGLDUSDT',
-        'SOLUSDT',
-        'ICXUSDT',
-        'STORJUSDT',
-        'BLZUSDT',
-        'UNIUSDT',
-        'AVAXUSDT',
-        'FTMUSDT',
-        'HNTUSDT',
-        'ENJUSDT',
-        'FLMUSDT',
-        'TOMOUSDT',
-        'RENUSDT',
-        'KSMUSDT',
-        'NEARUSDT',
-        'AAVEUSDT',
-        'FILUSDT',
-        'RSRUSDT',
-        'LRCUSDT',
-        'MATICUSDT',
-        'OCEANUSDT',
-        'CVCUSDT',
-        'BELUSDT',
-        'CTKUSDT',
-        'AXSUSDT',
-        'ALPHAUSDT',
-        'ZENUSDT',
-        'SKLUSDT',
-        'GRTUSDT',
-        '1INCHUSDT',
-        'AKROUSDT',
-        'CHZUSDT',
-        'SANDUSDT',
-        'ANKRUSDT',
-        'LUNAUSDT',
-        'BTSUSDT',
-        'LITUSDT',
-        'UNFIUSDT',
-        'DODOUSDT',
-        'REEFUSDT',
-        'RVNUSDT',
-        'SFPUSDT',
-        'XEMUSDT',
-        'COTIUSDT',
-        'CHRUSDT',
-        'MANAUSDT',
-        'ALICEUSDT',
-        'HBARUSDT',
-        'ONEUSDT',
-        'LINAUSDT',
-        'STMXUSDT',
-        'DENTUSDT',
-        'CELRUSDT',
-        'HOTUSDT',
-        'MTLUSDT',
-        'OGNUSDT',
-        'NKNUSDT',
-        'SCUSDT',
-        'DGBUSDT',
-        'SHIBUSDT',
-        'ICPUSDT',
-        'BAKEUSDT',
-        'GTCUSDT',
-        'TLMUSDT',
-        'IOTXUSDT',
-        'AUDIOUSDT',
-        'RAYUSDT',
-        'C98USDT',
-        'MASKUSDT',
-        'ATAUSDT',
-        'DYDXUSDT',
-        'XECUSDT',
-        'GALAUSDT',
-        'CELOUSDT',
-        'ARUSDT',
-        'KLAYUSDT',
-        'ARPAUSDT',
-        'CTSIUSDT',
-        'LPTUSDT',
-        'ENSUSDT',
-        'PEOPLEUSDT',
-        'ANTUSDT',
-        'ROSEUSDT',
-        'DUSKUSDT',
-        'BTTCUSDT',
-        'FLOWUSDT',
-        'IMXUSDT']
     const [downloaded, setDownloaded] = useState(false)
     let myCoins = []
-    console.log(myAllCoins)
+
 
 
     const computeExpensiveValue = () => {
@@ -153,7 +19,16 @@ export const App = () => {
                         res.data.asks.sort((a,b) => b[1] - a[1])
                         res.data.bids.sort((a,b) => b[1] - a[1])
                         axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${coinName}`)
-                           .then(resp => myCoins.push({name: coinName, asks: res.data.asks[0], bids: res.data.bids[0], price:resp.data.price}))
+                           .then(resp =>
+                               myCoins.push(
+                                   {
+                                       name: coinName,
+                                       asks: res.data.asks[0],
+                                       bids: res.data.bids[0],
+                                       asksVolume: parseInt(res.data.asks[0][0]*res.data.asks[0][1]),
+                                       bidsVolume: parseInt(res.data.bids[0][0]*res.data.bids[0][1]),
+                                       price:resp.data.price
+                                   }))
 
                     })
             }
@@ -167,9 +42,19 @@ export const App = () => {
     useEffect(() => {
         computeExpensiveValue()
     }, [setDownloaded])
+
     const calculateLevel = (price, askPrice) => {
         const result=(100 - (price / askPrice * 100))
         return result.toFixed(2)
+    }
+
+    const sortAsksCount = () => {
+        myAllCoins.sort((a, b) => b.asksVolume - a.asksVolume)
+        setDownloaded(!downloaded)
+    }
+    const sortBidsCount = () => {
+        myAllCoins.sort((a, b) => b.bidsVolume - a.bidsVolume)
+        setDownloaded(!downloaded)
     }
 
 
@@ -178,32 +63,28 @@ export const App = () => {
         <button onClick={() => {
             setTimeout(() => {
                 setDownloaded(!downloaded)
-                console.log(downloaded)
             },1000)
         }}>show</button>
+        <p>Количество монет : {myAllCoins.length}</p>
         <div style={{display:'flex', justifyContent:'space-between'}}>
-            <div style={{width:'11%', fontWeight:'700'}}>Название</div>
+            <div style={{width:'11%', fontWeight:'700', opacity:'0'}}>Название</div>
             <div style={{width:'11%', fontWeight:'700'}}>Цена покупки:</div>
-            <div style={{width:'11%', fontWeight:'700'}}>Количество:</div>
-            <div style={{width:'11%', fontWeight:'700'}}>Объем покупки в $</div>
+            <div style={{width:'11%', fontWeight:'700', cursor:'pointer'}} onClick={() => sortAsksCount()}>Объем покупки в $</div>
             <div style={{width:'11%', fontWeight:'700'}}>До уровня %</div>
             <div style={{width:'11%', fontWeight:'700'}}>Цена продажи:</div>
-            <div style={{width:'11%', fontWeight:'700'}}>Количество:</div>
-            <div style={{width:'11%', fontWeight:'700'}}>Объем продажи в $</div>
+            <div style={{width:'11%', fontWeight:'700', cursor:'pointer'}} onClick={() => sortBidsCount()}>Объем продажи в $</div>
             <div style={{width:'11%', fontWeight:'700'}}>До уровня %</div>
         </div>
         {
             myAllCoins ?
                 myAllCoins.map(coin =>
                 <div style={{marginTop:'3px', display:'flex', justifyContent:'space-between'}} key={coin.asks[1]}>
-                    <div style={{width:'11%'}}>{coin.name}</div>
+                    <div style={{width:'11%', fontWeight:'800'}}>{coin.name}</div>
                     <div style={{width:'11%', border: '1px solid green', paddingLeft: '15px'}}>{coin.asks[0]}</div>
-                    <div style={{width:'11%', border: '1px solid green', paddingLeft: '15px'}}>{coin.asks[1]}</div>
-                    <div style={{width:'11%', border: '1px solid green', paddingLeft: '15px'}}> {coin.asks[0]*coin.asks[1]}</div>
+                    <div style={{width:'11%', border: '2px solid green', paddingLeft: '15px'}}> {coin.asksVolume}</div>
                     <div style={{width:'11%', border: '1px solid green', paddingLeft: '15px'}}>{calculateLevel(coin.asks[0], coin.price)} %</div>
                     <div style={{width:'11%', border: '1px solid red', paddingLeft: '15px'}}>{coin.bids[0]}</div>
-                    <div style={{width:'11%', border: '1px solid red', paddingLeft: '15px'}}>{coin.bids[1]}</div>
-                    <div style={{width:'11%', border: '1px solid red', paddingLeft: '15px'}}> {coin.bids[0]*coin.bids[1]}</div>
+                    <div style={{width:'11%', border: '2px solid red', paddingLeft: '15px'}}> {coin.bidsVolume}</div>
                     <div style={{width:'11%', border: '1px solid red', paddingLeft: '15px'}}>{calculateLevel(coin.bids[0], coin.price)} %</div>
                 </div>
             )
